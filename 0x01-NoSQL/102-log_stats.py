993949096091f3b9d9b19c13d3c3020476f7a43f
YoussefKhaled03 <ykte03@gmail.com>
2024-08-28 03:35:53 +0300

    lol

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
 Provide stats about Nginx logs stored in MongoDB
-Database
+Database.
 """
 from pymongo import MongoClient
 
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}]
 
 
 def log_stats(mongo_collection, option=None):
@@ -28,6 +30,10 @@ def log_stats(mongo_collection, option=None):
         log_stats(nginx_collection, method)
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
+    print("IPs:")
+
+    for ip in mongo_collection.aggregate(PIPE):
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
 
 
 if __name__ == "__main__":
